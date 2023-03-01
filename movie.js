e188901dd2b4526153dff2f12b361f9a7060bbe3
prashantghadiali <er.prashantghadiali@gmail.com>
2023-03-01 19:07:32 +0530

    Add Promise

--- movie.js
+++ movie.js
@@ -1,3 +1,14 @@
-const search = document.getElementById("searchbar");
+const searchbar = document.getElementById("searchbar");
+const movielist = document.getElementById("movie-list");
+const movie = document.querySelectorAll(".movies");
+const title = document.getElementsByTagName("h6");
+
+let prom = fetch("http://www.omdbapi.com/?i=tt3896198&apikey=2982e8a6");
+
+prom.then((response) =>{
+    return response.json();
+}).then((response)=>{
+    console.log(response);
+})
 
 
