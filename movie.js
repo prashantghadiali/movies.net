e29231aec06f4d874b9fa2fef489a7085f5caad5
prashantghadiali <er.prashantghadiali@gmail.com>
2023-06-21 23:41:31 +0530

    working on Poster image query

--- movie.js
+++ movie.js
@@ -2,16 +2,97 @@ const searchbar = document.getElementById("searchbar");
 const movielist = document.getElementById("movie-list");
 const movie = document.querySelectorAll(".movies");
 const title = document.getElementsByTagName("h6");
-
-let prom = fetch("http://www.omdbapi.com/?i=tt3896198&apikey=2982e8a6");
-
-prom.then((response) =>{
-    console.log("get status: ", response.status);
-    return response.json();
-}).then((response)=>{
-    console.log(response);
-    console.log("get Title: ", response.Title);
-    console.log("get Year: ", response.Year);
-})
+const year = document.getElementById("year");
+const form = document.getElementById("form");
+const serch_poster = document.getElementById("ser_post");
+let file_home = document.getElementsByTagName("main");
 
 
+async function loadApi(searchData){
+    const prom = `https://omdbapi.com/?s=${searchData}&page=1&apikey=2982e8a6`;
+    const resp = await fetch(`${prom}`);
+    const data = await resp.json();
+    if(data.Response == "True"){
+        
+        // if any of the data is in data arr then it will call this
+        dataList(data.Search);
+    }
+    
+}
+
+function findBySearch(){
+    let searchData = searchbar.value.trim();
+    // getting the serch value to hit the api from this input data
+    if (searchData.length > 0){
+        movielist.classList.remove('hideClass');
+        loadApi(searchData);
+    } else {
+        movielist.classList.add('hideClass');
+    }
+    console.log(searchData);
+}
+
+function dataList(movieData){
+    movielist.innerHTML = "";
+    for(let mid = 0; mid < movieData.length; mid++){
+        let movieItem = document.createElement("div");
+        movieItem.dataset.id = movieData[mid].imdbID;
+        movieItem.classList.add('movies');
+        console.log(movieItem);
+        if(movieData[mid].Poster != "N/A" ){
+            mPoster = "./image/movies.net.png"
+            console.log("poster not avl", mPoster);
+        } else{
+            mPoster = movieData[mid].Poster;
+            console.log("poster", mPoster);
+            movieItem.innerHTML = `
+            <img src="https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg" alt="">
+
+                <img src="${mPoster}" alt="poster" id="ser_post" srcset="">
+                <div class="movie-details">
+                    <h6>${movieData[mid].Title}</h6>
+                    <p id="year">${movieData[mid].Year}</p>
+                </div>
+            `
+        }
+        movielist.appendChild(movieItem)
+    }
+}
+
+
+
+// prom.then((response) =>{
+//     console.log("get status: ", response.status);
+
+//     return response.json();
+//     }).then((response)=>{
+//         console.log(response);
+//         console.log("get Title: ", response.Title);
+//         console.log("get Year: ", response.Year);
+//         searchbar.addEventListener("click", ()=>{
+            
+//             title[0].innerText = response.Title;
+//             year.innerText = response.Year;
+//     });
+// });
+
+// function ser(){
+//     let options = (searchbar.value).trim();
+//     if(options.length > 0){
+//         searchbar.movielist.remove();
+//     }
+// }
+
+
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     file_home.innerHTML = '';
+
+//     const searchTerm = search.value;
+
+//     if (searchTerm) {
+//         show(SEARCHAPI + searchTerm);
+//         search.value = "";
+//     }
+// });
