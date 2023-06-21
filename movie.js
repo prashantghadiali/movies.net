bcb5d763957419877bfaf221233771b7ed6d9c02
prashantghadiali <er.prashantghadiali@gmail.com>
2023-06-21 23:41:04 +0530

    Working on Poster

--- movie.js
+++ movie.js
@@ -6,9 +6,12 @@ const title = document.getElementsByTagName("h6");
 let prom = fetch("http://www.omdbapi.com/?i=tt3896198&apikey=2982e8a6");
 
 prom.then((response) =>{
+    console.log("get status: ", response.status);
     return response.json();
 }).then((response)=>{
     console.log(response);
+    console.log("get Title: ", response.Title);
+    console.log("get Year: ", response.Year);
 })
 
 
