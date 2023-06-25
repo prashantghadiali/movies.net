83fbc465670d907c594026c22080d8171f92ae04
prashantghadiali <er.prashantghadiali@gmail.com>
2023-06-25 22:44:56 +0530

    working on selected movies page

--- movie.js
+++ movie.js
@@ -6,6 +6,7 @@ const year = document.getElementById("year");
 const form = document.getElementById("form");
 const serch_poster = document.getElementById("ser_post");
 let file_home = document.getElementsByTagName("main");
+const appendedResult = document.getElementsByClassName("append");
 
 
 async function loadApi(searchData){
@@ -38,10 +39,10 @@ function dataList(movieData){
         let movieItem = document.createElement("div");
         movieItem.dataset.id = movieData[mid].imdbID;
         movieItem.classList.add('movies');
-        console.log("movie item", movieItem);
+        // console.log("movie item", movieItem);
         if(movieData[mid].Poster != "N/A" ){
             var mPoster = movieData[mid].Poster;
-            console.log("poster", mPoster);
+            // console.log("poster", mPoster);
         } else{
             movieData[mid].Poster = "./image/movies.net.png"
         }
@@ -55,44 +56,63 @@ function dataList(movieData){
         `
         
         movielist.appendChild(movieItem)
-        console.log("movie list", movielist);
+        // console.log("movie list", movielist);
     }
+    loadDetails();
 }
 
+function loadDetails(){
+    const searchList = movielist.querySelectorAll(".movies");
+    searchList.forEach(searchitem => {
+        searchitem.addEventListener('click', async () => {
+            // console.log(searchitem);
+            movielist.classList.add('hideClass');
+            searchbar.value = '';
+            // fetch for clicked file
+            const clickData = await fetch(`https://omdbapi.com/?i=${searchitem.dataset.id}&apikey=2982e8a6`)
+            const clickDataDetails = await clickData.json();
+            console.log("Data Selected",clickDataDetails);
+            displaySelected(clickDataDetails);
+        })
+    })
+}
 
+let append = document.getElementById("append");
+var selectedTitle = "Title";
+var selectedPoster = "Poster";
+var selectedYear = "Year";
+function displaySelected(selected){
+    localStorage.setItem(selectedTitle, selected.Title);
+    localStorage.setItem(selectedPoster , selected.Poster);
+    localStorage.setItem(selectedYear , selected.Year);
+    location.pathname = "/movie_page/movies.html";
+    append.addEventListener("onload",  () => {
+        console.log("Data Selected",movieDataDetails);
+        let appendItem = document.createElement("div");
+        appendItem.classList.add('card');
+        appendItem.classList.add('text-center');
+        console.log(appendItem);
+        append.innerHTML += `
+                <div class="card-body">
+                    <img src="${(localStorage.Poster != "N/A") ? localStorage.Poster : "/image/movies.net.png"}" class="" alt="Poster">
+                    <br>
+                    <br>
+                    <h5 class="card-title">Title: ${localStorage.Title}</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <p class="card-title">Genre : </p>
+                    <a href="#" class="btn btn-primary">Add To Favourits</a>
+                </div>
+                <div class="card-footer text-muted">
+                    Year : ${localStorage.Year}
+                </div>
+        `
+    })
 
-// prom.then((response) =>{
-//     console.log("get status: ", response.status);
+}
 
-//     return response.json();
-//     }).then((response)=>{
-//         console.log(response);
-//         console.log("get Title: ", response.Title);
-//         console.log("get Year: ", response.Year);
-//         searchbar.addEventListener("click", ()=>{
-            
-//             title[0].innerText = response.Title;
-//             year.innerText = response.Year;
-//     });
-// });
-
-// function ser(){
-//     let options = (searchbar.value).trim();
-//     if(options.length > 0){
-//         searchbar.movielist.remove();
-//     }
-// }
-
-
-
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     file_home.innerHTML = '';
-
-//     const searchTerm = search.value;
-
-//     if (searchTerm) {
-//         show(SEARCHAPI + searchTerm);
-//         search.value = "";
-//     }
-// });
+// click outside of search list -> list disapeears
+window.addEventListener("click", (e) => {
+    if(e.target.className != "movie-list"){
+        movielist.classList.add('hideClass');
+    }
+})
